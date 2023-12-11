fa19d277e5f56766e8ccb33bacc0c7d2af6038fe
Giulio Borzetta <giulioborz@gmail.com>
2023-12-11 15:20:04 +0100

    eliminazione commenti

--- src/js/api.js
+++ src/js/api.js
@@ -1,6 +1,5 @@
 import '../css/styles.css';
 
-//cerca
 const apiUrl = 'https://openlibrary.org/search.json?title=';
 let search = document.getElementById("searchButton");
 const resultContainer = document.getElementById("risultati-api");
@@ -8,30 +7,26 @@ const resultDescription = document.getElementById("descriptionBooks");
 
 search.addEventListener("click", function(){
 
-// Ottieni il valore dell'input
 const inputValue = document.getElementById("input").value.trim();
 
-// Verifica se l'input è valido
 if (!inputValue) {
 console.error('Please enter a valid title');
 return;
 }
 
-// Effettua la chiamata all'API utilizzando la Fetch API
 fetch(`${apiUrl}${inputValue}`)
 .then(response => {
-    // Controlla se la richiesta ha avuto successo (status code 200-299)
+
     if (!response.ok) {
         throw new Error(`Error in API request: ${response.status}`);
     }
-    // Parsifica la risposta JSON
+
     return response.json();
 })
 .then(response => {
 
-    // Verifica se la risposta contiene l'array "docs"
     if (response.docs && response.docs.length > 0) {
-        // I dati sono disponibili qui
+
         for (let i = 0; i < Math.min(10, response.docs.length); i++) {
 
             const title = document.createElement("h2");
@@ -53,13 +48,10 @@ fetch(`${apiUrl}${inputValue}`)
 
                     const apiUrl = `https://openlibrary.org${response.docs[i].key}.json`;
 
-                    // Esegui la richiesta fetch
                     fetch(apiUrl)
                     .then(response => response.json())
                     .then(data => {
-                        // La struttura di 'data' dipenderà dai dettagli restituiti dall'API di Open Library
 
-                        // Verifica se ci sono dati disponibili
                         if(!data.description){
                             const noDescription = document.createElement("p");
                             noDescription.textContent = "There is no description for this book";
@@ -75,11 +67,11 @@ fetch(`${apiUrl}${inputValue}`)
                             descriptionText.textContent = data.description;
                             resultDescription.appendChild(descriptionText);
                         }
-                        // Estrai informazioni specifiche come titolo e autore
+
                         const line = document.createElement("hr");
                         line.classList.add("marginHr");
                         resultDescription.appendChild(line);
-                        // Fai qualcosa con le informazioni estratte
+
                         console.log(data);
                     })
                     .catch(error => {
@@ -100,43 +92,39 @@ fetch(`${apiUrl}${inputValue}`)
     }
 })
 .catch(error => {
-    // Gestisci gli errori
     console.error('Error calling API:', error);
 });
 
 });
 
 
-//categoria
+
+
 const apiUrlCategory = 'https://openlibrary.org/subjects/';
 let searchCategory = document.getElementById("submitSearchCategory");
 
 searchCategory.addEventListener("click", function(){
 
-// Ottieni il valore dell'input
 const inputValue = document.getElementById("inputSearchCategory").value;
 
-// Verifica se l'input è valido
 if (!inputValue) {
 console.error('Please enter a valid category');
 return;
 }
 
-// Effettua la chiamata all'API utilizzando la Fetch API
 fetch(`${apiUrl}${inputValue}`)
 .then(response => {
-// Controlla se la richiesta ha avuto successo (status code 200-299)
+
 if (!response.ok) {
 throw new Error(`Error in API request: ${response.status}`);
 }
-// Parsifica la risposta JSON
+
 return response.json();
 })
 .then(response => {
 
-// Verifica se la risposta contiene l'array "docs"
 if (response.docs && response.docs.length > 0) {
-// I dati sono disponibili qui
+
 for (let i = 0; i < Math.min(10, response.docs.length); i++) {
 
     const title = document.createElement("h2");
@@ -158,13 +146,10 @@ for (let i = 0; i < Math.min(10, response.docs.length); i++) {
 
             const apiUrl = `https://openlibrary.org${response.docs[i].key}.json`;
 
-            // Esegui la richiesta fetch
             fetch(apiUrl)
             .then(response => response.json())
             .then(data => {
-                // La struttura di 'data' dipenderà dai dettagli restituiti dall'API di Open Library
 
-                // Verifica se ci sono dati disponibili
                 if(!data.description){
                     const noDescription = document.createElement("p");
                     noDescription.textContent = "There is no description for this book";
@@ -184,9 +169,7 @@ for (let i = 0; i < Math.min(10, response.docs.length); i++) {
                 const line = document.createElement("hr");
                 line.classList.add("marginHr");
                 resultDescription.appendChild(line);
-                // Estrai informazioni specifiche come titolo e autore
 
-                // Fai qualcosa con le informazioni estratte
                 console.log(data);
             })
             .catch(error => {
@@ -205,7 +188,7 @@ console.error('The JSON response does not contain a valid "docs" array.');
 }
 })
 .catch(error => {
-// Gestisci gli errori
+
 console.error('Error calling API:', error);
 });
 
@@ -213,35 +196,35 @@ console.error('Error calling API:', error);
 
 
 
-//per cercare dai 11 a 20
+
+
 const apiUrl2 = 'https://openlibrary.org/search.json?title=';
 let moreSearch= document.getElementById("moreSearchButton");
 
 moreSearch.addEventListener("click", function(){
 
-// Ottieni il valore dell'input
+
 const inputValue = document.getElementById("input").value.trim();
 
-// Verifica se l'input è valido
+
 if (!inputValue) {
 console.error('Please enter a valid title');
 return;
 }
 
-// Effettua la chiamata all'API utilizzando la Fetch API
 fetch(`${apiUrl2}${inputValue}`)
 .then(response => {
-    // Controlla se la richiesta ha avuto successo (status code 200-299)
+
     if (!response.ok) {
         throw new Error(`Error in API request: ${response.status}`);
     }
-    // Parsifica la risposta JSON
+
     return response.json();
 })
 .then(response => {
-    // Verifica se la risposta contiene l'array "docs"
+
     if (response.docs && response.docs.length > 0) {
-        // I dati sono disponibili qui
+
         for (let i = 11; 11 < i < Math.max(21, response.docs.length); i++) {
             const title = document.createElement("h2");
             title.textContent = response.docs[i].title;
@@ -263,13 +246,10 @@ fetch(`${apiUrl2}${inputValue}`)
 
                     const apiUrl = `https://openlibrary.org${response.docs[i].key}.json`;
 
-                    // Esegui la richiesta fetch
                     fetch(apiUrl)
                     .then(response => response.json())
                     .then(data => {
-                        // La struttura di 'data' dipenderà dai dettagli restituiti dall'API di Open Library
 
-                        // Verifica se ci sono dati disponibili
                         if(!data.description){
                             const noDescription = document.createElement("p");
                             noDescription.textContent = "There is no description for this book";
@@ -285,11 +265,11 @@ fetch(`${apiUrl2}${inputValue}`)
                             descriptionText.textContent = data.description;
                             resultDescription.appendChild(descriptionText);
                         }
-                        // Estrai informazioni specifiche come titolo e autore
+
                         const line = document.createElement("hr");
                         line.classList.add("marginHr");
                         resultDescription.appendChild(line);
-                        // Fai qualcosa con le informazioni estratte
+
                         console.log(data);
                     })
                     .catch(error => {
@@ -312,7 +292,7 @@ fetch(`${apiUrl2}${inputValue}`)
     }
 })
 .catch(error => {
-    // Gestisci gli errori
+
     console.error('Error calling API:', error);
 });
 
